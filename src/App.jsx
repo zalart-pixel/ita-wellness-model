8685befa3123d2bb165690d94ce303d7806c6b23
zalart-pixel <zalitisarturs@gmail.com>
2025-12-22 23:04:56 +0200

    Switch to ITAWellnessLeanModel component for deployment
    Update App.jsx to use the new ITAWellnessLeanModel component with LEAN vs FULL comparison and crisis scenario analysis

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import React from 'react'
-import ITAWellnessModel from './ita-wellness-model'
+import ITAWellnessLeanModel from './ITAWellnessLeanModel'
 
 export default function App() {
-  return (
-    <div style={{
-      minHeight: '100vh',
-      background: '#0f172a',
-      color: '#e5e7eb'
-    }}>
-      <ITAWellnessModel />
-    </div>
-  )
+    return (
+          <ITAWellnessLeanModel />
+        )
 }
