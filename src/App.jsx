1a12e59f6d0de65286a5ff6b343cbd16ddcf5147
zalart-pixel <zalitisarturs@gmail.com>
2025-12-22 22:04:04 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ITAWellnessModel from './ita-wellness-model'
+
+export default function App() {
+  return (
+    <div style={{
+      minHeight: '100vh',
+      background: '#0f172a',
+      color: '#e5e7eb'
+    }}>
+      <ITAWellnessModel />
+    </div>
+  )
+}
